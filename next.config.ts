48801fa262dffe3bfae54dfb3f4aa9e18759c2f7
Saksit Jittasopee <saksitjittasopee@gmail.com>
2025-12-01 11:42:36 +0700

    fix next.config

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'dist',
+  basePath: '/saksit-website',
   images: {
     unoptimized: true,
   },
